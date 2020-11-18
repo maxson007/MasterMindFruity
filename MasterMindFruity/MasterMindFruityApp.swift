d512c28f0159880ebd614a315025b185022d3db6
Aymane <danielayman@gmail.com>
2020-11-18 23:17:03 +0100

    Initial Commit

---
+++ MasterMindFruity/MasterMindFruityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterMindFruityApp.swift
+//  MasterMindFruity
+//
+//  Created by Aymane DANIEL on 18/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct MasterMindFruityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
