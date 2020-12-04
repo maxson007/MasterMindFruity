45ed41e02ab64baa3369e8ab101e1437a778ef8c
Aymane <danielayman@gmail.com>
2020-12-04 21:09:25 +0100

    gameover view correctif

--- MasterMindFruity/View/GameOverView.swift
+++ MasterMindFruity/View/GameOverView.swift
@@ -12,7 +12,7 @@ struct GameOverView: View {
     var title: String
     var message: String
     @Binding var isGameOver: Bool
-    var secretCode: [Int]=[1,2,3,4]
+    var secretCode: [Int]
     var basket: FruitBasket = FruitBasket()
     var body: some View {
         
@@ -21,7 +21,7 @@ struct GameOverView: View {
                     .font(Font.custom("Juicy Fruity", size: 30, relativeTo: .title))
                     .foregroundColor(Color(#colorLiteral(red: 0.6950075626, green: 0.8562402129, blue: 0.008320940658, alpha: 1))).padding()
                 Text(message).font(.largeTitle).fontWeight(.semibold).foregroundColor(Color(#colorLiteral(red: 0.2365519702, green: 0.7098777294, blue: 0.2898065746, alpha: 1))).multilineTextAlignment(.center).padding()
-                Text("Le code secret est :").font(.title3).fontWeight(.semibold).multilineTextAlignment(.center)
+                Text("Le code secret etait :").font(.title3).fontWeight(.semibold).multilineTextAlignment(.center)
                 HStack {
                     
                     ForEach(secretCode, id: \.self) { idFruit in
@@ -55,6 +55,6 @@ struct GameOverView: View {
 
 struct GameOverView_Previews: PreviewProvider {
     static var previews: some View {
-        GameOverView(title: "Gagne !!", message: "Vous avez gagné la partie 🥳", isGameOver: .constant(true),secretCode: [1,2,3,4])
+        GameOverView(title: "Gagne !!", message: "Vous avez gagné la partie 🥳", isGameOver: .constant(true),secretCode: [1,6,3,4])
     }
 }
