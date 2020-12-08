861646e2de1f2f6c9cf81c2d083b6cb84cdafc5b
Aymane <danielayman@gmail.com>
2020-12-08 16:56:17 +0100

    correctif bug d'affichage sur ipod7: affichage pas responsive

--- MasterMindFruity/View/ResultRowView.swift
+++ MasterMindFruity/View/ResultRowView.swift
@@ -14,12 +14,12 @@ struct ResultRowView: View {
     var body: some View {
         HStack{
             Text("\(result.id)").foregroundColor(Color.blue).multilineTextAlignment(.center).font(Font.custom("Juicy Fruity", size: 15, relativeTo: .title)).rotationEffect(.radians(.pi))
-                .scaleEffect(x: -1, y: 1, anchor: .center)
+                .scaleEffect(x: -1, y: 1, anchor: .center).aspectRatio(contentMode: .fit)
             Divider()
             ForEach(result.userSecretCode, id: \.self) { idFruit in
                 HStack {
                     Image(basket.fruits[idFruit-1].name).resizable().aspectRatio(contentMode: .fit).rotationEffect(.radians(.pi))
-                        .scaleEffect(x: -1, y: 1, anchor: .center).frame(width: 55)
+                        .scaleEffect(x: -1, y: 1, anchor: .center)
                 }
                 
             }
@@ -28,23 +28,23 @@ struct ResultRowView: View {
                 HStack {
                     Circle()
                         .fill(result.resultPlaced.indices.contains(0) ?  result.resultPlaced[0] : Color.gray)
-                        .frame(width: 30, height: 30)
+                        .frame(width: 30, height: 30).aspectRatio(contentMode: .fit)
                     
                     Circle()
                         .fill(result.resultPlaced.indices.contains(1) ?  result.resultPlaced[1] : Color.gray)
-                        .frame(width: 30, height: 30)
+                        .frame(width: 30, height: 30).aspectRatio(contentMode: .fit)
                     
                 }
                 
                 HStack {
                     Circle()
                         .fill(result.resultPlaced.indices.contains(2) ?  result.resultPlaced[2] : Color.gray)
-                        .frame(width: 30, height: 30)
+                        .frame(width: 30, height: 30).aspectRatio(contentMode: .fit)
                     
                     
                     Circle()
                         .fill(result.resultPlaced.indices.contains(3) ?  result.resultPlaced[3] : Color.gray)
-                        .frame(width: 30, height: 30)
+                        .frame(width: 30, height: 30).aspectRatio(contentMode: .fit)
                     
                 }
                 
