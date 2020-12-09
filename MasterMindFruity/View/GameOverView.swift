39d3d9b8531889e42fc47dbd2d907294eb1d40da
Aymane <danielayman@gmail.com>
2020-12-09 10:17:35 +0100

    Correctif: affichage text Ipod7

--- MasterMindFruity/View/GameOverView.swift
+++ MasterMindFruity/View/GameOverView.swift
@@ -23,7 +23,11 @@ struct GameOverView: View {
                 Text(title)
                     .font(Font.custom("Juicy Fruity", size: 30, relativeTo: .title))
                     .foregroundColor(isPlayerSuccess ? Color(#colorLiteral(red: 0.6950075626, green: 0.8562402129, blue: 0.008320940658, alpha: 1)) : Color(#colorLiteral(red: 0.9593037963, green: 0.05458746105, blue: 0.004058180377, alpha: 1))).padding()
+                    .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                    .fixedSize(horizontal: false, vertical: true)
                 Text(message).font(.largeTitle).fontWeight(.semibold).foregroundColor(isPlayerSuccess ? Color(#colorLiteral(red: 0.2365519702, green: 0.7098777294, blue: 0.2898065746, alpha: 1)) : Color(#colorLiteral(red: 0.9593037963, green: 0.05458746105, blue: 0.004058180377, alpha: 1))).multilineTextAlignment(.center).padding()
+                    .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                    .fixedSize(horizontal: false, vertical: true)
                 Text("Le code secret etait :").font(.title3).fontWeight(.semibold).multilineTextAlignment(.center)
                 HStack {
                     
@@ -48,7 +52,7 @@ struct GameOverView: View {
                 }
             }
             .padding()
-            .frame(width: screenSize.width * 0.99, height: screenSize.height * 0.6)
+            .frame(width: screenSize.width * 0.99, height: screenSize.height * 0.7)
             .background(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
             .clipShape(RoundedRectangle(cornerRadius: 20.0, style: .continuous))
             .offset(y: isGameOver ? 0 : screenSize.height)
